a6b4ef432db63fb87faaa6c51e7887fe2d52e455
nik <example.com>
2022-10-23 23:59:13 +0300

    added additional entities and repos

---
+++ src/main/java/guru/springframework/entities/Difficulty.java
@@ -0,0 +1,7 @@
+package guru.springframework.entities;
+
+public enum Difficulty {
+    EASY,
+    MODERATE,
+    HARD
+}
--- src/main/java/guru/springframework/entities/Recipe.java
+++ src/main/java/guru/springframework/entities/Recipe.java
@@ -20,9 +20,16 @@ public class Recipe {
     private Byte[] image;
     @OneToOne(cascade = CascadeType.ALL)
     private Note note;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+                joinColumns = @JoinColumn(name = "recipe_id"),
+                inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -110,5 +117,21 @@ public class Recipe {
 
     public void setNote(Note note) {
         this.note = note;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
